cc69fac585407dcea188d4d30e3508c0363c8542
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2022-01-31 09:29:13 -0600

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+export default {
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Ticketr",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Ticketr"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://modules.nuxtjs.org
+   */
+  modules: [],
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
+  css: ["@/static/assets/bootstrap.min.css", "@/static/assets/var.css"],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
+   */
+  plugins: ["~/plugins/firebase.js"]
+};
---
+++ plugins/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA77SCrUyT-tasrTkG-v4crRskvmEiyUig",
+  authDomain: "ticketr-app.firebaseapp.com",
+  projectId: "ticketr-app",
+  storageBucket: "ticketr-app.appspot.com",
+  messagingSenderId: "755995675808",
+  appId: "1:755995675808:web:3904441a35a94e2c6c8dc5",
+  measurementId: "G-BX823GDD6R"
+};
+
+initializeApp(firebaseConfig);
